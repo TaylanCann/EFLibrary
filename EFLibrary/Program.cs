9fc35ce3d8ca00fcf49167b0f6395e809a5f38e9
TaylanCann <taylancanh@gmail.com>
2022-03-16 15:36:02 +0300

    SmthngAdded

---
+++ EFLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
