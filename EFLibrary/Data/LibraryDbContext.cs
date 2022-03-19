459bd443996d8e7ca4931f96bff2264b15257d18
TaylanCann <taylancanh@gmail.com>
2022-03-19 23:00:27 +0300

    AddSmthng

---
+++ EFLibrary/Data/LibraryDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EFLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFLibrary.Data
+{
+    public class LibraryDbContext :DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }  
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserBook> UserBooks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connection => 
+        }
+    }
+}
